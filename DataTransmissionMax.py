a698b90876da729d2f9afa045a28e42fd76cb49a
Wilfrido TA <a01664769@tec.mx>
2024-11-25 23:09:05 -0600

    non use files deleted

--- DataTransmissionMax.py
+++
@@ -1,6 +0,0 @@
-"""
-2. The program must also read another square matrix of N x N data representing the maximum capacity of data transmission between neighborhood i and neighborhood j. 
-Since we are working with cities with a large number of electromagnetic fields, which can generate interference, estimates have already been made and are reflected in this matrix.
-
-The company wants to know the maximum information flow from the initial node to the final node. This should also be displayed in the standard output.
-"""
--- EfficientWiring.py
+++
@@ -1,5 +0,0 @@
-"""
-1. read an input file containing the information of a graph represented in the form of an adjacency matrix with weighted graphs. 
-The weight of each edge is the distance in kilometers from neighborhood to neighborhood, where it is feasible to put wiring.
-The program must display which is the optimal way to wire with optical fiber connecting neighborhoods in such a way that information can be shared between any two neighborhoods [hint: Dijkstra's algorithm].
-"""
